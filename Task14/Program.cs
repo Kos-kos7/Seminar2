40b3168724123e17fd94435e912799538e100b70
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-14 15:35:58 +0500

    Задачи 11,14 и 16

---
+++ Task14/Program.cs
@@ -0,0 +1,6 @@
+﻿//принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
---
+++ Task16/Program.cs
@@ -0,0 +1,8 @@
+﻿//принимает 2 числа и выводит, является ли одно квадратом другого
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 * number1 == number2 || number2*number2 == number1) Console.WriteLine("Yes");
+else Console.WriteLine("No");
