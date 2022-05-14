6c346cdb54e194f917523f24c724779904bf07bc
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-14 13:40:12 +0500

    Seminar2

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿//принимает 2 числа и выводит, является ли второе кратное первому
+int number1 = new Random().Next(100,1000);
+int number2 = new Random().Next(1,10);
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+
+if (number1 % number2 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
