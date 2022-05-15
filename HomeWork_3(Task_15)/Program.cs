6f6da8d3c97773975d6887f93413b80ed640ef6e
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-15 11:50:25 +0500

    Про выходные дни

---
+++ HomeWork_3(Task_15)/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7");
+string numWeek = Console.ReadLine();
+
+if(numWeek == "6") || (numWeek == "7") Console.WriteLine("Ура выходной");
+else Console.WriteLine("Опять на работу");  
+
