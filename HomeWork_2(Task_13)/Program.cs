fef26129ccedcf62c0e956c77fd71e8b30e90410
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-15 11:37:19 +0500

    Домашка про оставить треью цифру

---
+++ HomeWork_2(Task_13)/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int num1 = Math.Abs(num);
+
+string text = num1.ToString();
+
+Console.WriteLine("Оставляем третью цифру из числа: " + num + " -> " + text[2]);
